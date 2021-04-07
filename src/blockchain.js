53c1d276e5db665d68a3be649b870223d84fab1b
bl0upibl0up <moreau.ambroise@gmail.com>
2021-04-07 17:38:28 +0200

    change time

--- src/blockchain.js
+++ src/blockchain.js
@@ -126,7 +126,7 @@ class Blockchain {
         return new Promise(async (resolve, reject) => {
             let messageTime = parseInt(message.split(':')[1]);
             let currentTime = parseInt(new Date().getTime().toString().slice(0,-3));
-            if (currentTime - messageTime > 300000){
+            if (currentTime - messageTime > 300){
                 //console.log('currentTime:', currentTime);
                 //console.log('messageTime: ', messageTime);
                 resolve('the star is too old');
